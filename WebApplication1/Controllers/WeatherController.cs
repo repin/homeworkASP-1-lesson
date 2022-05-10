007b716b98b557748a49bc9b40568020d25cc8e5
repin <sasharepin@Mail.ru>
2022-05-10 09:11:47 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/WeatherController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System.Linq;
+using System;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly WeatherHolder _weatherHolder;
+        public WeatherController(WeatherHolder holder)
+        {
+            _weatherHolder = holder;
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read([FromQuery]DateTime timeStart, [FromQuery]DateTime timeEnd)
+        {
+            return Ok(_weatherHolder.TakeTempToSpan(timeStart,timeEnd));
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery]DateTime time, [FromQuery]float temp)
+        {
+            _weatherHolder.Add(time, temp);
+            return Ok();
+        }
+
+        [HttpPut("edit")]
+        public IActionResult  Edit([FromQuery]DateTime time,[FromQuery]float temp)
+        {
+            _weatherHolder.Edit(time, temp);
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery]DateTime time)
+        {
+            _weatherHolder.Delete(time);
+            return Ok();
+        }
+
+    }
+
+}
---
+++ WebApplication1/Models/WeatherHolder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Models
+{
+    public class WeatherHolder
+    {
+        public WeatherHolder()
+        {
+            _weather = new Dictionary<DateTime, float>();
+        }
+
+        private Dictionary<DateTime,float> _weather;
+
+        public void Add (DateTime dateTime, float tempCels)
+        {
+            _weather.Add(dateTime, tempCels);
+        }
+
+        public void Delete(DateTime dateTime)
+        {
+            if (_weather.ContainsKey(dateTime))
+            {
+                _weather.Remove(dateTime);
+            }
+        }
+
+        public void Edit(DateTime dateTime, float tempCels)
+        {
+            if (_weather.ContainsKey(dateTime))
+            {
+                _weather[dateTime] = tempCels;
+            }
+        }
+
+        public string TakeTempToSpan(DateTime startDateTime, DateTime endDateTime)
+        {
+            List<string> tempToInterval = new List<string>();
+            if (_weather is null) return null;
+            foreach (DateTime dateTime in _weather.Keys)
+            {
+                if(startDateTime <= dateTime && dateTime <= endDateTime)
+                {
+                    tempToInterval.Add(_weather[dateTime].ToString() +" - " +dateTime.ToString());
+                }
+            }
+            return string.Join(',',tempToInterval.ToArray());
+        }
+    }
+}
